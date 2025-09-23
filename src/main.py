8a8a2bd8001593f2b864adedc7d039bcf1fa7868
Matope <guilletope510@gmail.com>
2025-09-24 01:39:21 +0200

    primeros cambio, .gitignore, main, env, requirements

--- src/main.py
+++ src/main.py
@@ -24,8 +24,8 @@ logger=logging.getLogger("adult-income")
 run_name = f"run-{datetime.now().strftime('%Y%m%d-%H%M%S')}"
 
 # MLflow config
-MLFLOW_URI = "http://localhost:5000"
-EXPERIMENT_NAME = "adult-income"
+MLFLOW_URI = "sqlite:////mlflow/mlflow.db"  # nota los 4 '/' para ruta absoluta
+EXPERIMENT_NAME = "adult-income-guillermo3"
 
 mlflow.set_tracking_uri(MLFLOW_URI)
 mlflow.set_experiment(EXPERIMENT_NAME)
